12175f99c13517b1a07751bc37ea27c35e1d23a0
Silver-Reels <124486812+Silver-Reels@users.noreply.github.com>
2023-05-11 09:00:18 +0100

    grain logic fix, mobile fixes

--- s1lver.js
+++ s1lver.js
@@ -1,10 +1,22 @@
 { //"BACK TO TOP" BUTTON FUNCTIONALITY/////////////////////////////////////////////////////////////////
 const topButtonDiv=document.querySelector(".TopButton");
-window.onscroll = function() {TopButtonFunc()};  //when scrolling happens in window, activate function
+window.onscroll = function(e){
+  TopButtonFunc();
+  readjustMinHeight();
+};
 
 function TopButtonFunc(){
   if (window.pageYOffset>92) {topButtonDiv.classList.add("TopButtonActive")} //if scrolled down below 92px
-  else                       {topButtonDiv.classList.remove("TopButtonActive")}}
+  else                       {topButtonDiv.classList.remove("TopButtonActive")}
+}
+
+function readjustMinHeight(){ //trying to fix issues with body bg being broken on mobile
+  document.body.style.minHeight=`${document.body.scrollHeight}px`;
+}
+//console.log(e);
+//document.body.style.minHeight=`${e.currentTarget.screen.availHeight}px;`
+//availWidth
+//availHeight
 }//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 {//URL DELAYER: USE IN HTML WITH href="javascript:delay('url')"/////////////////////////////
@@ -37,6 +49,7 @@ function loadingCalc(){ //function that sets the text of loading screen progress
   `${((imgLoaded/allimageslength)*33.4)}vw`;         //these will often NOT reach 100%, as small images sometimes load faster than this JS script, so can't be iterated
 }
 
+//DOCUMENT BODY ONLOAD/////////////////////////////////////////////////////////////////////////////////////////////
 //once <body> i.e. entire page is fully loaded
 document.body.onload=()=>{
   loadDiv.classList.add("loadingDivGone");                                //loadingscreen goes away
@@ -46,13 +59,13 @@ document.body.onload=()=>{
   if (nonreel){nonreel.classList.remove("reel-moving-in")};
   loadDiv.style.filter="blur(0.9vw)";                                     //add "motion" blur when loaddiv is to move away
 
-
-//console.log(`body loaded`);
+  
+  //console.log(`body loaded`);
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 //once active navigation bar button is clicked (i.e. page will load away)
-
 //for each active nav button clicked
 navButtons.forEach((button)=>
 button.onclick=function(){
@@ -78,88 +91,87 @@ button.onclick=function(){
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //IMAGE VIEWER FUNCTIONALITY
-
-const reelImg= Array.from(reel.getElementsByClassName("reel-img"));       //needs "reel" const to be defined above
+let reelImg=null;
+if (reel){reelImg=      Array.from(reel.getElementsByClassName("reel-img"))}; //needs "reel" const to be defined above
 const viewerDivParent=  document.querySelector(".viewerDivParent");
-const viewer=           document.querySelector(".viewer");                //viewer class, for the image in viewer functionality
+const viewer=           document.querySelector(".viewer");                    //viewer class, for the image in viewer functionality
 const viewertext=       document.querySelector(".viewertext");
-
 let lastImgClicked=""; //this is here so the other functions like "hide viewer" can track 'img'
-if (reelImg){
-reelImg.forEach((img,i)=>
-img.onclick=function(){
-    lastImgClicked=img; //letting 'img' be tracked elsewhere, no need to use this const here tho
 
+if (reelImg){
+  reelImg.forEach((img,i)=>
+  img.onclick=function(){
+    lastImgClicked=img; //letting 'img' be tracked elsewhere, no need to use this const here tho
+    
     document.body.style.overflowY="hidden"; //no scrolling
     
     //anims for removing clicked thumbnail from view
     let styleAddress=img.parentElement.style;
     styleAddress.left="98vw";
     styleAddress.zIndex="2";
-    styleAddress.transform="rotate(-5deg)";
-    styleAddress.filter="blur(0.9vw)";
-
-    //'src' of <img> in class=viewer is (default empty) grabbed from HTML of image clicked in 'reel-img'.
-    //should have a 'x-vsrc' that can then be displayed by the viewer
-    viewer.src="";                 //reset viewer image
-    viewer.src=img.attributes.getNamedItem("x-vsrc").value; 
-    
-    viewertext.innerText=          //grabs reeltitle text and puts it in viewertext
-    `FILE #${i+1>9? i+1 : "0"+(i+1)}: ${img.parentElement.getElementsByClassName("reel-title")[0].innerText}`;
-    viewer.style.filter="blur(0)"; //for motion blur
-    
-    //adds visibility to viewerDivParent (do this last)
-    viewerDivParent.classList.add("viewerDivParentVisible");
+      styleAddress.transform="rotate(-5deg)";
+      styleAddress.filter="blur(0.9vw)";
+      
+      viewer.src="";                 //reset viewer image
+      //'src' of <img> in class=viewer is (default empty) grabbed from x-vsrc of image clicked in 'reel-img'.
+      viewer.src=img.attributes.getNamedItem("x-vsrc").value;
+      
+      viewertext.innerText=          //grabs reel-title text and puts it in viewertext
+      `FILE #${i+1>9? i+1 : "0"+(i+1)}: ${img.parentElement.getElementsByClassName("reel-title")[0].innerText}`;
+      viewer.style.filter="blur(0)"; //for motion blur
+      
+      //adds visibility to viewerDivParent (do this last)
+      viewerDivParent.classList.add("viewerDivParentVisible");
+    }
+    )
   }
-  )
-}
-
-//hide viewer
-viewerDivParent.onclick=function(){hideViewer()};
-//hide viewer with ESC
-document.onkeydown=function(e){
-  if(e.key=="Escape" && viewerDivParent.className=="viewerDivParent viewerDivParentVisible"){
-    hideViewer()}};
+  
+  //hide viewer
+  if (viewerDivParent){viewerDivParent.onclick=function(){hideViewer()};}
+  //hide viewer with ESC
+  document.onkeydown=function(e){
+    if(e.key=="Escape" && (viewerDivParent) && viewerDivParent.className=="viewerDivParent viewerDivParentVisible"){
+      hideViewer()}};
+      
+      function hideViewer(){viewerDivParent.classList.remove("viewerDivParentVisible");
+      viewer.style.filter="blur(0.9vw)"; //viewer "motion" blur
+      lastImgClicked.parentElement.style.left="0"; //animations for the thumbnail
+      lastImgClicked.parentElement.style.filter="";
+      document.body.style.overflowY="auto"; //scrolling is back
+      setTimeout(function(){lastImgClicked.parentElement.style.transform=""},150);
+      setTimeout(function(){lastImgClicked.parentElement.style.zIndex=""},500);
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////
     
-    function hideViewer(){viewerDivParent.classList.remove("viewerDivParentVisible");
-                      viewer.style.filter="blur(0.9vw)"; //viewer "motion" blur
-                      lastImgClicked.parentElement.style.left="0"; //animations for the thumbnail
-                      lastImgClicked.parentElement.style.filter="";
-                      document.body.style.overflowY="auto"; //scrolling is backaaaaa
-                      setTimeout(function(){lastImgClicked.parentElement.style.transform=""},150);
-                      setTimeout(function(){lastImgClicked.parentElement.style.zIndex=""},500);
-}
-///////////////////////////////////////////////////////////////////////////////////////
-
-//INKR
-
-if (window.location.pathname.toLowerCase()==`/inkr.html` || window.location.pathname.toLowerCase()==`/inkr`){ //only run if we're on the correct HTML page
+    //INKR
+    
+    if (window.location.pathname.toLowerCase()==`/inkr.html` || window.location.pathname.toLowerCase()==`/inkr`){ //only run if we're on the correct HTML page
   console.log(`Activating external INKR`);
   if (reel){reel.style.height=0;reel.style.width=0;reel.style.padding=0;reel.style.margin=0};
   if (reelImg){reelImg[0].style.height=0;reelImg[0].style.width=0};
   if (viewerDivParent){viewerDivParent.style.width=0;viewerDivParent.style.height=0;viewerDivParent.style.display="none"};
   if (nonreel){nonreel.style.display="block"};
-
+  
   const inkrForm=     document.querySelector(".typeForm");
   const counter=      document.querySelector(".typeCounter");
   const inkrTextField=document.querySelector("input[type=text");
-
+  
   inkrForm.onsubmit=function(e){         
     e.preventDefault();
-
+    
     if (inkrTextField.value.length<=40){
-    const iframeDiv=   document.getElementsByTagName("iframe")[0];
-    const iframeWindow=iframeDiv.contentWindow;
-    let charDivs=      Array.from(iframeWindow.document.querySelectorAll(".char"));
-    let charParent=    iframeWindow.document.querySelector(".charParent");
-    let charCount=     iframeWindow.document.querySelector(".charParent").children;
-    let charHeight=    50;
-
-    iframeWindow.document.body.children[0].innerHTML=iframeWindow.populateHTML(inkrTextField.value);
-    charDivs.forEach(x=>x.style.height=`${charHeight*0.6}px`);
-    iframeWindow.setWidthAll();
-    iframeWindow.setTail(charCount);
-    iframeWindow.animateHTML(charParent);
+      const iframeDiv=   document.getElementsByTagName("iframe")[0];
+      const iframeWindow=iframeDiv.contentWindow;
+      let charDivs=      Array.from(iframeWindow.document.querySelectorAll(".char"));
+      let charParent=    iframeWindow.document.querySelector(".charParent");
+      let charCount=     iframeWindow.document.querySelector(".charParent").children;
+      let charHeight=    50;
+      
+      iframeWindow.document.body.children[0].innerHTML=iframeWindow.populateHTML(inkrTextField.value);
+      charDivs.forEach(x=>x.style.height=`${charHeight*0.6}px`);
+      iframeWindow.setWidthAll();
+      iframeWindow.setTail(charCount);
+      iframeWindow.animateHTML(charParent);
     }
     else alert('Above INKR demo character limit!');
   }
@@ -167,12 +179,12 @@ if (window.location.pathname.toLowerCase()==`/inkr.html` || window.location.path
   window.onkeyup=function(){
     counter.innerHTML=`<p>${40-inkrTextField.value.length}/40</p>`;
   }
-
+  
   const typeInput=document.querySelector(".typeInput");
   let focused=false;
   typeInput.onclick=function(){typeInput.style.width=`42vw`;typeInput.style.boxShadow=`0 0 0.15vw #D7D7D7`;focused=true};
-     window.onclick=function(){if (focused==true){focused=false} else {typeInput.style.width=`26vw`;typeInput.style.boxShadow=`0 0 0.4vw #D7D7D7`}};
-
+  window.onclick=function(){if (focused==true){focused=false} else {typeInput.style.width=`26vw`;typeInput.style.boxShadow=`0 0 0.4vw #D7D7D7`}};
+  
 }
 // INTRO PAGE ICONS///////////////////////////////////////////////////////
 let softwareIcon1=   Array.from(document.querySelectorAll(".software-icon-1"));
@@ -227,8 +239,8 @@ if (window.location.pathname.toLocaleLowerCase()==`/` || window.location.pathnam
   const leetTextFun=document.getElementById("leet-fun");
   const leetClass=Array.from(document.getElementsByClassName("leet-stats"));
   const aboutTextQuery=document.querySelector(".about-text-query");
-
-/*  
+  
+  /*  
   let leetcodeURL=`https://leetcode.com/graphql/`;
   let leetcodeQuery={
     "query":"query skillStats($username: String!) {matchedUser(username: $username) {tagProblemCounts {advanced {tagName problemsSolved} intermediate {tagName problemsSolved} fundamental { tagName problemsSolved } } } }",
@@ -236,11 +248,11 @@ if (window.location.pathname.toLocaleLowerCase()==`/` || window.location.pathnam
     "operationName": "skillStats"
   };
   const init={method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(leetcodeQuery)};
-
+  
   WE ARE USING THESE IN THE SERVERLESS FUNCTION
 
   REQUEST WITH CONSOLE:
-curl --request POST \
+  curl --request POST \
   --url https://leetcode.com/graphql \
   --header 'Content-Type: application/json' \
   --data '{
@@ -248,38 +260,39 @@ curl --request POST \
     "variables":{"username": "Silver-Reels"},
     "operationName": "skillStats"
   }'
+  
+  */
+ let leetcodeStats={};
+ let leetFetched=false;
+ 
+ introicons[1].onclick = async () => {
+   if (!leetFetched){
+     leetFetched=true;
+     try{
+       aboutTextQuery.style.filter=`blur(2vw) opacity(0)`;
+       aboutTextQuery.style.pointerEvents=`none`;
+       //console.log(`calling...`);
+       const response = await fetch("/.netlify/functions/fetchLeetCode") //netlify serverless function
+       .then(rawResponse=>rawResponse.json())
+       
+       leetcodeStats=response.data.data.matchedUser.tagProblemCounts;
+       
+       //strings
+       let advS=``;
+       let intS=``;
+       let funS=``;
+       
+       //arrays
+       leetcodeStats.advanced.forEach(obj=>advS+=`x${obj.problemsSolved} ${obj.tagName}\n`);
+       leetcodeStats.intermediate.forEach(obj=>intS+=`x${obj.problemsSolved} ${obj.tagName}\n`);
+        leetcodeStats.fundamental.forEach(obj=>funS+=`x${obj.problemsSolved} ${obj.tagName}\n`);
 
-*/
-  let leetcodeStats={};
-  let leetFetched=false;
-
-  introicons[1].onclick = async () => {
-    if (!leetFetched){
-      leetFetched=true;
-      try{
-        aboutTextQuery.style.filter=`blur(2vw) opacity(0)`;
-        //console.log(`calling...`);
-        const response = await fetch("/.netlify/functions/fetchLeetCode") //netlify serverless function
-        .then(rawResponse=>rawResponse.json())
-
-        leetcodeStats=response.data.data.matchedUser.tagProblemCounts;
         
-        //strings
-        let advS=``;
-        let intS=``;
-        let funS=``;
-
-        //arrays
-        leetcodeStats.advanced.forEach(obj=>advS+=`x${obj.problemsSolved} ${obj.tagName}\t\n`);
-        leetcodeStats.intermediate.forEach(obj=>intS+=`x${obj.problemsSolved} ${obj.tagName}\t\n`);
-        leetcodeStats.fundamental.forEach(obj=>funS+=`x${obj.problemsSolved} ${obj.tagName}\t\n`);
-
-
         //<p>'s
         leetTextAdv.innerText=advS;
         leetTextInt.innerText=intS;
         leetTextFun.innerText=funS;
-
+        
         //effect to uncover <p>
         leetClass[0].classList.add("leet-stats-shown");  
       }
@@ -289,22 +302,22 @@ curl --request POST \
   ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   //INTRO PARALLAX
-
+  
   let layer1=document.querySelector(".parallax-layer-1").style; //moves most, on top
   let layer2=document.querySelector(".parallax-layer-2").style;
   let layer3=document.querySelector(".parallax-layer-3").style;
   let layer4=document.querySelector(".parallax-layer-4").style;
   let layer5=document.querySelector(".parallax-layer-5").style; //moves least, on bottom
-
+  
   let mod1=13;let mod2= 8;let mod3= 5;let mod4= 3;let mod5= 2;  //fibonacci. could do modX & modY too
-
+  
   function parallax(x,y){ //mousemove event; e.clientX, e.clientY
     //let maxX= document.body.clientWidth; let centerX=maxX/2;
     //let maxY=document.body.clientHeight; let centerY=maxY/2;
-   
+    
     //(center - currentMouse) / totalScreen = offset from center as % of total screen
     //this value * modifier = how much to translate(x%,y%)
-    
+
     //layer1.transform=`translate(${((centerX-x)/maxX)*modX1}%,${((centerY-y)/maxY)*modY1}%)`;
     //math optimize: (center-current)/2*center = 0.5-(current/max)
     let calcX=0.5-(x/document.body.clientWidth);
@@ -332,16 +345,9 @@ document.onmousemove=function(e){
   }
 }
 
-//315 to 370
-let bg=document.getElementsByTagName("body");
-let gradientAngle=315;
-function gradientShift(value){
-  let newValue;
-  if (value===315){newValue=370}
-  if (value===370){newValue=315}
-  bg.style.backgroundImage=`url(/bg.webp), linear-gradient(${newValue}deg, rgba(18,18,18,1) 84%, rgba(48,48,48,1) 100%)`
-}
-//setInterval();
+
 
 /////////////////////////////////////////////////////////////////
+
+
 
